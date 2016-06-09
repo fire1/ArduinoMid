13537ea8c2c9f70933072119e09c875f86bcdfbc
Angel Zaprianov <fire1@abv.bg>
2016-06-09 11:38:03 +0300

    Adding steering wheel media controls  convert to  Sony Alpine standards

--- ArduinoMid.ino
+++ ArduinoMid.ino
@@ -1,4 +1,5 @@
 /*
+---------------------------------------------------
     Arduino MID
 
      Author: Angel Zaprianov <me@fire1.eu>
@@ -7,15 +8,46 @@
 
      For the full copyright and license information, please view the LICENSE
      file that was distributed with this source code.
-
+---------------------------------------------------
 */
 
 //
-// Display configs
-#include <LiquidCrystal.h> // includes the LiquidCrystal Library
+// Includes the LiquidCrystal Library
+#include <LiquidCrystal.h>
 //
 // Creates an LC object. Parameters: (rs, enable, d4, d5, d6, d7)
 LiquidCrystal lcd(1, 2, 4, 5, 6, 7);
+/*
+    Steering wheel buttons to Sony Alpine
+
+            resistance	voltage
+---------------------------------------------------
+src      	2200	    4.1
+att	        4400	    3.47
+off	        35	        4.98
+vol down	23850	    1.47
+vol up	    17000	    1.85
+seek down	12000	    2.25
+seek up	    8800	    2.65
+back	    6660	    3
+front	    33940	    1.13
+bottom	    48800	    0.84
+---------------------------------------------------
+ Sony Voltages                                  */
+const int sonySrc = 41; //4.1v
+const int sonyAtt = 34;
+const int sonyOff = 49;
+const int sonyVolDw = 14;
+const int sonyVolUp = 18;
+const int sonySeekDw = 26;
+const int sonySeekUp = 22;
+const int sonyBack = 29;
+const int sonyFront = 11;
+const int sonyBottom = 8;
+//
+// Output for Sony Alpine emulation
+const int sonyPin = A0;
+const int sonyShiftPin = A2;
 //
 // Defines min/max menu amplitude
 const int maxMenuNumber = 2;
@@ -30,7 +62,7 @@ int buttonStateUp = 0;   // current state of the button
 int buttonStateDw = 0;   // previous state of the button
 //
 // Navigation state value
-int navigationState = 0;
+int navigationState = -1;
 
 
 void setup() {
@@ -41,6 +73,9 @@ void setup() {
     //
     // Initializes the interface to the LCD screen, and specifies the dimensions (width and height) of the display }
     lcd.begin(16,2);
+    //
+    // Define Alpine Pin
+    pinMode(alpPin, OUTPUT);
 }
 
 void loop() {
@@ -60,29 +95,15 @@ void loop() {
         case 2:
 
           break;
+
+        case 0:
+            handleDefaultMenu();
+        break;
         //
         // MID Introduction ...
-        case 0:
         default:
-         lcd.print("Welcome to "); // Prints "Arduino" on the LCD
-         delay(3000); // 3 seconds delay
-         lcd.setCursor(11,0); // Sets the location at which subsequent text written to the LCD will be displayed
-         lcd.print("Opel");
-         delay(3000);
-         lcd.setCursor(1,2);
-         lcd.print("Astra");
-         delay(3000);
-         lcd.clear(); // Clears the display
-         lcd.blink(); //Displays the blinking LCD cursor
-         delay(4000);
-         lcd.setCursor(2,1);
-         lcd.print("Loading ...");
-         delay(3000);
-         lcd.noBlink(); // Turns off the blinking LCD cursor
-         lcd.cursor(); // Displays an underscore (line) at the position to which the next character will be written
-         delay(4000);
-         lcd.noCursor(); // Hides the LCD cursor
-         lcd.clear(); // Clears the LCD screen
+            handleIntroduceMenu();
+            navigationState++;
         break;
       }
 
@@ -91,10 +112,11 @@ void loop() {
 }
 
 /**
- *
+        Handle MID's steering buttons
  */
 int handleButtonNavigation(int stateUp,int stateDw){
-
+    Serial.println(stateUp);
+    Serial.println(stateDw);
 
 
     if (buttonStateUp == HIGH) {
@@ -105,13 +127,55 @@ int handleButtonNavigation(int stateUp,int stateDw){
         navigationState--;
     }
 
-
-
+    // Debug info ...
+    Serial.println(navigationState);
 }
 
 /**
- *
+        Handle Drive menu
  */
-char handleDriveMode(){
+void handleDriveMenu(){
 
 }
+/**
+        Handle Default menu
+ */
+void handleDefaultMenu(){
+
+}
+/**
+        Handle Introduce menu
+ */
+void handleIntroduceMenu(){
+     lcd.print("Welcome to "); // Prints "Arduino" on the LCD
+     delay(3000); // 3 seconds delay
+     lcd.setCursor(11,0); // Sets the location at which subsequent text written to the LCD will be displayed
+     lcd.print("Opel");
+     delay(3000);
+     lcd.setCursor(1,2);
+     lcd.print("Astra");
+     delay(3000);
+     lcd.clear(); // Clears the display
+     lcd.blink(); //Displays the blinking LCD cursor
+     delay(4000);
+     lcd.setCursor(2,1);
+     lcd.print("Loading ...");
+     delay(3000);
+     lcd.noBlink(); // Turns off the blinking LCD cursor
+     lcd.cursor(); // Displays an underscore (line) at the position to which the next character will be written
+     delay(4000);
+     lcd.noCursor(); // Hides the LCD cursor
+     lcd.clear(); // Clears the LCD screen
+}
+
+/**
+        Handle Steering wheel media buttons
+ */
+void handleSteeringToSony(int voltage, boolean shift){
+
+     // Debug info ...
+     Serial.println(voltage);
+     Serial.println(shift);
+}
+
+
