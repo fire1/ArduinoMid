ea21f4c959729f065849aa70e4ed2361f21e2ca3
Angel <azaprianov@mw-medianetworks.eu>
2017-05-10 14:28:06 +0300

    Attach  interrupt fixture

--- MID/lib/CarSens.h
+++ MID/lib/CarSens.h
@@ -610,10 +610,9 @@ void EngSens_catchEcuHits() {
   * @param pinTarget
   */
 void CarSens::setupRpmSens(uint8_t pinTarget) {
-    pinMode(pinTarget, INPUT_PULLUP);
-    analogWrite(pinTarget, 255);
-//    analogWrite(pinTarget, 0);// Drop down pin voltage TODO test mode!
     attachInterrupt(digitalPinToInterrupt (pinTarget), EngSens_catchRpmHits, FALLING);
+    pinMode(pinTarget, INPUT_PULLUP);
+//    analogWrite(pinTarget, 128);// Fix voltage over pin  TODO test mode!
 };
 
 /**
@@ -621,10 +620,9 @@ void CarSens::setupRpmSens(uint8_t pinTarget) {
   * @param pinTarget
   */
 void CarSens::setupVssSens(uint8_t pinTarget) {
-    pinMode(pinTarget, INPUT_PULLUP);
-    analogWrite(pinTarget, 255);
-//    analogWrite(pinTarget, 0);// Drop down pin voltage TODO test mode!
     attachInterrupt(digitalPinToInterrupt (pinTarget), EngSens_catchVssHits, FALLING);
+    pinMode(pinTarget, INPUT_PULLUP);
+//    analogWrite(pinTarget, 128);// Fix voltage over pin  TODO test mode!
 };
 
 /**
@@ -632,10 +630,9 @@ void CarSens::setupVssSens(uint8_t pinTarget) {
   * @param pinTarget
   */
 void CarSens::setupEcuSens(uint8_t pinTarget) {
-    pinMode(pinTarget, INPUT_PULLUP);
-
-    analogWrite(pinTarget, 255);// Drop down pin voltage TODO test mode!
     attachInterrupt(digitalPinToInterrupt(pinTarget), EngSens_catchEcuHits, FALLING);
+    pinMode(pinTarget, INPUT_PULLUP);
+//    analogWrite(pinTarget, 128);// Fix voltage over pin  TODO test mode!
 };
 
 /**
