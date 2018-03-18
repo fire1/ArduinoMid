4e3c8b7df652a62742a9060bc9015dff2ba89ff9
Angel Zaprianov <fire1@abv.bg>
2018-03-18 22:50:09 +0200

    Commit after tests

---
+++ tests/LogCard/LogCard.ino
@@ -0,0 +1,61 @@
+#include <Arduino.h>
+
+#include <SD.h>
+#include <SPI.h>
+
+
+#ifndef __SD_H__
+
+#include "../../libraries/SD/src/SD.h"
+#include "../../libraries/SD/src/utility/SdFat.h"
+
+#endif
+
+
+const int chipSelect = 22;
+const char *logFilename = "mid.log";
+
+
+File logFile;
+
+void setup() {
+
+
+    Serial.begin(115200);
+    // see if the card is present and can be initialized:
+    if (!SD.begin(chipSelect)) {
+        Serial.println("Card failed, or not present");
+        // don't do anything more:
+        return;
+    }
+    Serial.println("card initialized.");
+
+
+    logFile = SD.open(logFilename);
+    if (logFile) {
+        Serial.println("mid.log: ");
+        // read from the file until there's nothing else in it:
+        while (logFile.available()) {
+            Serial.write(logFile.read());
+        }
+        // close the file:
+        logFile.close();
+    } else {
+        // if the file didn't open, print an error:
+        Serial.println("error opening mid.log");
+    }
+
+
+    logFile = SD.open(logFilename, FILE_WRITE);
+
+    if (logFile) {
+        logFile.println("test");
+        logFile.close();
+    }
+
+}
+
+void loop() {
+
+
+}
