aedc046101bb9fc03ab5f02996b100a1c500c2b2
Angel <azaprianov@mw-medianetworks.eu>
2018-03-26 15:24:27 +0300

    Remove junk

--- MID/glob.h
+++ MID/glob.h
@@ -7,9 +7,9 @@
 
 #include <Arduino.h>
 #include "lib/AmpTime.h"
-#include "lib/debug.h"
+#include "lib/Debug.h"
 #include "lib/MenuBackend.h"
-#include "lib/debug.h"
+#include "lib/Debug.h"
 //
 //
 /***************************************************************************
