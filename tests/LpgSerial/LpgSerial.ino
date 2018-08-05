925c8195d10103034804367de5dbd1a63afdd775
Angel Zaprianov <fire1@abv.bg>
2018-08-05 13:10:05 +0300

    new LPG test

--- tests/LpgSerial/LpgSerial.ino
+++ tests/LpgSerial/LpgSerial.ino
@@ -1,58 +1,92 @@
 
 #include <Arduino.h>
 
-#define BitVal(data, y) ( (data>>y) & 1)
+#define LPG_INPUT 19
+#define LPG_TIMEOUT  8
+#define LPG_BITS 14
+#define LPG_BYTE_HIGH 6 // time for HIGH 1 byte
+#define LPG_TOLERANCE 1 // tolerance time
 
-const uint8_t pinInput = 17;
-
-boolean serial2Change = false;
-uint8_t serial2Length;
-boolean serial2Readed = false;
-uint8_t serial2Offset;
-unsigned long serial2LastTime;
-
-uint32_t dataBuff;
-
-void serialListener() {
-
-    int pinState = digitalRead(pinInput);
+class LpgSerial {
 
 
-    if (pinState == HIGH) {
-        serial2Change = true;
-        serial2Length = 0;
-    }
-
-    if (pinState == LOW && serial2Change) {
-        serial2Length = (uint8_t)(millis() - serial2LastTime); //if it is low, end the time
-        serial2LastTime = millis();
-        serial2Change = false;
-
-        if (serial2Length >= 1 && serial2Length <= 6) { // 5ms read
-            dataBuff += 0 << serial2Offset;
-            serial2Offset++;
-
-        }
+    boolean lowEnds = true, receive = false;
+    uint8_t sync = 0, offset = 0;
+    int lastState = LOW;
+    unsigned long lowBegin = 0;
+    uint32_t buffer, transmission;
 
 
-        if (serial2Length > 6 && serial2Length <= 8) { // 7ms read
-            dataBuff += 1 << serial2Offset;
-            serial2Offset++;
-        }
-
-        //
-        // 110100000001 // act LPG full
-
-        if (serial2Length > 8 || serial2Offset > 11) { // 15 downs
-            serial2Readed = true;
-            serial2Length = 0;
-            serial2Offset = 0;
-        }
-
+private:
+    boolean isStart() {
+        return lastState == HIGH;
     }
 
 
-}
+    uint8_t getInputLen() {
+        unsigned long highLen = 0;
+        int in = digitalRead(LPG_INPUT);
+
+        if (lowBegin == 0 && in == LOW && isStart()) {
+            lowBegin = millis();
+            lowEnds = false;
+        }
+
+        if (!lowEnds && in == LOW && isStart()) {
+            highLen = millis() - lowBegin;
+        }
+
+        if (highLen > LPG_TIMEOUT) {
+            return 0;
+        }
+
+        lastState = in;
+        return (uint8_t) highLen;
+    }
+
+public:
+
+    void begin() {
+//        if (sync == 0) {
+//            sync = uint8_t(pulseIn(LPG_INPUT, HIGH, LPG_TIMEOUT) / 1000);
+//        }
+    }
+
+    void listener() {
+        uint8_t len = getInputLen();
+
+        if (offset == 0 && receive != 0) {
+            buffer = '\0';
+            receive = false;
+        }
+
+        if (len > LPG_BYTE_HIGH && len < (LPG_BYTE_HIGH + LPG_TOLERANCE)) {
+            buffer += B1;
+            offset++;
+        } else {
+            buffer += B0;
+            offset++;
+        }
+
+        if (offset >= LPG_BITS) {
+            offset = 0;
+            receive = true;
+            transmission = buffer;
+        }
+
+
+    }
+
+    boolean isReceive() {
+        return receive;
+    }
+
+    uint32_t getData() {
+        return transmission;
+    }
+
+
+};
 
 // LPG
 // 2221 2201
@@ -62,27 +96,16 @@ void serialListener() {
 // 1501 1521 C01 1C00 201
 // 110000000001
 
+LpgSerial lpg;
 
 void setup() {
-    pinMode(pinInput, INPUT);
-    digitalWrite(pinInput, LOW);
-
-//    attachInterrupt(digitalPinToInterrupt(pinInput), serialEvent2, FALLING );
-    Serial.begin(115200);
-    Serial.println("Beginning ... ");
+    lpg.begin();
 }
 
 
 void loop() {
-
-    serialListener();
-
-    if (serial2Readed) {
-        Serial.println(BitVal(dataBuff, 0));
-        Serial.print(F("BIN "));
-        Serial.print(dataBuff, BIN);
-        dataBuff = 0;
-        Serial.println();
-        serial2Readed = false;
+    lpg.listener();
+    if (lpg.isReceive()) {
+        Serial.println(lpg.getData(), BIN);
     }
 }
