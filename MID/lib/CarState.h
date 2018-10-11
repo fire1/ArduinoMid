5037d8d2c8047cd9e2a2f1156f5f1e1a0eea24da
Angel Zaprianov <fire1@abv.bg>
2018-10-11 16:19:33 +0300

    CarState small increase of verification time

--- MID/lib/CarState.h
+++ MID/lib/CarState.h
@@ -9,7 +9,7 @@
 #include "CarSens.h"
 
 #ifndef CAR_STT_A1_ALERT
-#define CAR_STT_A1_ALERT 13 // a wait minute - default 10 / last 20
+#define CAR_STT_A1_ALERT 14 // a wait minute - default 10 / last 13
 #endif
 
 #ifndef CAR_STT_A2_ALERT
